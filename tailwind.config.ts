731e2bc922f392e6345edd421e189cdb2eeb7caa
Perpetual <perpetualobuamu@gmail.com>
2024-04-01 13:46:12 +0100

    added color codes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,20 +13,50 @@ const config = {
     container: {
       center: true,
       padding: "2rem",
+
+      screens: {
+        sm: '280px',
+        sml: '320px',
+        smd: '425px',
+        mdm: '370px',
+        mdl: '600px',
+        md: '768px',
+        mdg: '900px',
+        lg: '1024px',
+        xl: '1280px',
+        xxl: '1440px',
+        '2xl': '1550px',
+        '3xl': '2560px',
+      },
+  
     },
-    screens: {
-      sm: '280px',
-      sml: '320px',
-      smd: '425px',
-      mdm: '370px',
-      mdl: '600px',
-      md: '768px',
-      mdg: '900px',
-      lg: '1024px',
-      xl: '1280px',
-      xxl: '1440px',
-      '2xl': '1550px',
-      '3xl': '2560px',
+    color: {
+      ba_primary: '#1F2B6C',
+      ba_secondary: '#0524C9',
+      ba_accent: '#E1EEFF',
+      ba_dark: '#1A1A1A',
+      ba_body_text: '#404040',
+      ba_subtle: '#808080',
+      ba_light: '#FFFFFF',
+      ba_success: '#31D0AA',
+      ba_error: '#E81010',
+      ba_warning: '#EED202'
+    },
+    fontSize: {
+      textLarge: '4rem',
+      headerOne: '3rem',
+      headerTwo: '2.5rem',
+      headerThree: '2rem',
+      headerFour: '1/75rem',
+      headerFive: '1.375rem',
+      headerSix: '1.25rem',
+      bodySize: '1rem',
+      smallSize: '0.875rem'
+    },
+    fontWeight: {
+      ba_large: '700',
+      ba_medium: '600',
+      ba_normal: '500'
     },
     extend: {
       keyframes: {
