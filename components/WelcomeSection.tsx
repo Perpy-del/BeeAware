e47e3e9ef3bbc5a5275112f6c090331392580ccc
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-04-03 08:57:55 +0100

    style: framer motion for the welcome section

--- components/WelcomeSection.tsx
+++ components/WelcomeSection.tsx
@@ -1,37 +1,68 @@
 import Image from 'next/image';
 import React from 'react';
 import ButtonComponent from './ButtonComponent';
+import { motion } from 'framer-motion';
+import { countOneVariants, countTwoVariants, countThreeVariants } from '@/data';
 
 type Props = {};
 
 const WelcomeSection = (props: Props) => {
   return (
-    <div className='flex items-center gap-20 pb-24 px-20 text-baBody dark:text-baLight'>
-      <div className='w-[35%]'>
+    <div className="flex items-center gap-20 pb-24 px-20 text-baBody dark:text-baLight">
+      <div className="w-[35%]">
         <Image
           width={480}
           height={480}
           src="/second_section_pic.jpg"
           alt="welcome message picture"
-          className='object-cover object-center'
-          loading='lazy'
+          className="object-cover object-center"
+          loading="lazy"
         />
       </div>
-      <div className='w-[60%] space-y-6'>
-        <h1 className='text-headerTwo font-ba_large leading-6 text-baDark dark:text-baSubtle'>Welcome to BeeAware</h1>
-        <p className='text-headerFive'>
+      <div className="w-[60%] space-y-6">
+        <h1 className="text-headerTwo font-ba_large leading-6 text-baDark dark:text-baSubtle">
+          Welcome to BeeAware
+        </h1>
+        <p className="text-headerFive">
           Through BeeAware, users can access consultations with healthcare
           professionals, informative articles on a wide range of sexual health
           topics, anonymous forums, and chatrooms for open discussions, and
           discover nearby STI testing locations for proactive health management.
         </p>
-        <div className='flex items-center gap-10'>
-          <div className='flex flex-col'><h1 className='text-headerTwo text-baSecondary font-ba_large leading-10'>3K+</h1><span className='text-baDark text-smallSize'>HAPPY CLIENTS</span></div>
-          <div className='flex flex-col'><h1 className='text-headerTwo text-baSecondary font-ba_large leading-10'>150K+</h1><span className='text-baDark text-smallSize'>PROFESSIONAL DOCTORS</span></div>
-          <div className='flex flex-col'><h1 className='text-headerTwo text-baSecondary font-ba_large leading-10'>15K+</h1><span className='text-baDark text-smallSize'>INFORMATIVE ARTICLES</span></div>
-          
-        </div>
-        <ButtonComponent btnText='Learn More' width='w-[200px]' variant='primary' />
+        <motion.div
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.8 }}
+          className="flex items-center gap-10"
+        >
+          <div className="flex flex-col">
+            <motion.h1 variants={countOneVariants} className="text-headerTwo text-baSecondary font-ba_large leading-10">
+              3K+
+            </motion.h1>
+            <motion.span variants={countOneVariants} className="text-baDark dark:text-baLight text-smallSize">HAPPY CLIENTS</motion.span>
+          </div>
+          <div className="flex flex-col">
+            <motion.h1 variants={countTwoVariants} className="text-headerTwo text-baSecondary font-ba_large leading-10">
+              150K+
+            </motion.h1>
+            <motion.span variants={countTwoVariants} className="text-baDark dark:text-baLight text-smallSize">
+              PROFESSIONAL DOCTORS
+            </motion.span>
+          </div>
+          <div className="flex flex-col">
+            <motion.h1 variants={countThreeVariants} className="text-headerTwo text-baSecondary font-ba_large leading-10">
+              15K+
+            </motion.h1>
+            <motion.span variants={countThreeVariants} className="text-baDark dark:text-baLight text-smallSize">
+              INFORMATIVE ARTICLES
+            </motion.span>
+          </div>
+        </motion.div>
+        <ButtonComponent
+          btnText="Learn More"
+          width="w-[200px]"
+          variant="primary"
+        />
       </div>
     </div>
   );
--- data.ts
+++ data.ts
@@ -1,146 +1,193 @@
+import { Variants } from 'framer-motion';
+
 export const navData = [
-    {
-        name: 'Home',
-        link: '/'
-    },
-    {
-        name: 'About',
-        link: '/about'
-    },
-    {
-        name: 'Services',
-        link: '/services'
-    },
-    {
-        name: 'Blogs',
-        link: '/blogs'
-    },
-    {
-        name: 'Contact',
-        link: '/contact'
-    }
-]
+  {
+    name: 'Home',
+    link: '/',
+  },
+  {
+    name: 'About',
+    link: '/about',
+  },
+  {
+    name: 'Services',
+    link: '/services',
+  },
+  {
+    name: 'Blogs',
+    link: '/blogs',
+  },
+  {
+    name: 'Contact',
+    link: '/contact',
+  },
+];
 
 export const moreAboutData = [
-    {
-        header: 'Education',
-        text: 'Accessible and Reliable Information about STIs, Contraception and Sexual Health. '
-    },
-    {
-        header: 'Consultation',
-        text: 'Talk to skilled personnels and make good decisons concerning your health. '
-    },
-    {
-        header: 'Testing',
-        text: 'Locate nearby testing facilities conveniently and with ease. '
-    },
-    {
-        header: 'Community',
-        text: 'Join a supportive community of individuals navigating similar experiences. '
-    },
-]
+  {
+    header: 'Education',
+    text: 'Accessible and Reliable Information about STIs, Contraception and Sexual Health. ',
+  },
+  {
+    header: 'Consultation',
+    text: 'Talk to skilled personnels and make good decisons concerning your health. ',
+  },
+  {
+    header: 'Testing',
+    text: 'Locate nearby testing facilities conveniently and with ease. ',
+  },
+  {
+    header: 'Community',
+    text: 'Join a supportive community of individuals navigating similar experiences. ',
+  },
+];
 
 export const articlesData = [
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-    {
-        image: '/article_image.jpg',
-        focus: 'Sexual Health',
-        date: 'January 2nd, 2024',
-        topic: 'STDs: What Are They and How Do You Get Them?',
-        sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).'
-    },
-]
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+  {
+    image: '/article_image.jpg',
+    focus: 'Sexual Health',
+    date: 'January 2nd, 2024',
+    topic: 'STDs: What Are They and How Do You Get Them?',
+    sub: 'The number of cases of sexually transmitted diseases (STDs) — now more commonly referred to as sexually transmitted infections (STIs) — in the United States hit an all-time high in 2019, according to data released on April 13, 2021, by the Centers for Disease Control and Prevention (CDC).',
+  },
+];
 
 export const testimonialData = [
-    {
-        words: 'BeeAware has been an invaluable resource for me as I navigate my sexual health journey.The informative articles are well-written and easy to understand, and the anonymous forums provide a supportive community where I feel comfortable sharing my experiences and learning from others.',
-        testifier: 'John Smith',
-        job: 'College Student'
+  {
+    words:
+      'BeeAware has been an invaluable resource for me as I navigate my sexual health journey.The informative articles are well-written and easy to understand, and the anonymous forums provide a supportive community where I feel comfortable sharing my experiences and learning from others.',
+    testifier: 'John Smith',
+    job: 'College Student',
+  },
+  {
+    words:
+      "I'm so grateful for BeeAware! I was able to find nearby STI testing locations quickly and easily, thanks to their platform. The consultations with healthcare professionals gave me peace of mind and helped me make informed decisions about my sexual health.",
+    testifier: 'Anonymous',
+    job: 'Graduate',
+  },
+  {
+    words:
+      'BeeAware has exceeded my expectations in every way. The wealth of information available on their platform is impressive, and I love that I can access it all from the comfort of my own home.',
+    testifier: 'Anonymous',
+    job: 'Graduate',
+  },
+];
+
+export const countOneVariants: Variants = {
+  offscreen: {
+    y: 300,
+  },
+  onscreen: {
+    y: 0,
+    transition: {
+      type: 'spring',
+      bounce: 0.7,
+      duration: 1,
     },
-    {
-        words: "I'm so grateful for BeeAware! I was able to find nearby STI testing locations quickly and easily, thanks to their platform. The consultations with healthcare professionals gave me peace of mind and helped me make informed decisions about my sexual health.",
-        testifier: 'Anonymous',
-        job: 'Graduate'
+  },
+};
+
+export const countTwoVariants: Variants = {
+  offscreen: {
+    y: 300,
+  },
+  onscreen: {
+    y: 0,
+    transition: {
+      type: 'spring',
+      bounce: 0.7,
+      duration: 1.5,
     },
-    {
-        words: "BeeAware has exceeded my expectations in every way. The wealth of information available on their platform is impressive, and I love that I can access it all from the comfort of my own home.",
-        testifier: 'Anonymous',
-        job: 'Graduate'
+  },
+};
+
+export const countThreeVariants: Variants = {
+  offscreen: {
+    y: 300,
+  },
+  onscreen: {
+    y: 0,
+    transition: {
+      type: 'spring',
+      bounce: 0.7,
+      duration: 2,
     },
-]
+  },
+};
