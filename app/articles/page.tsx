686eb66fc5074fc32accade91820e5887fb3f8a3
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-04-11 19:23:36 +0100

    chore: add scroll button

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -4,26 +4,28 @@ import PopularArticleComponent from '@/components/ArticlesComponents/PopularArti
 import PopularTopics from '@/components/ArticlesComponents/PopularTopics';
 import RecommendedArticles from '@/components/ArticlesComponents/RecommendedArticles';
 import SearchArticleComponent from '@/components/ArticlesComponents/SearchArticleComponent';
+import ScrollButton from '@/components/ScrollButton';
 
 type Props = {};
 
 const ArticlesPage = (props: Props) => {
   return (
-    <div id='articles'>
-      <SearchArticleComponent />
-      <PopularArticleComponent />
+    <>
+      <div id="articles">
+        <SearchArticleComponent />
+        <PopularArticleComponent />
 
-      {/* Popular Topic Section */}
-      <div className="sm:pb-14 lg:pb-24">
-        <h1 className="sm:text-headerFour lg:text-headerTwo sm:font-ba_normal lg:font-ba_large sm:pb-6 lg:pb-5 sm:mx-5 lg:mx-20 3xl:mx-40">
-          Popular Topics
-        </h1>
-        <PopularTopics />
+        {/* Popular Topic Section */}
+        <div className="sm:pb-14 lg:pb-24">
+          <h1 className="sm:text-headerFour lg:text-headerTwo sm:font-ba_normal lg:font-ba_large sm:pb-6 lg:pb-5 sm:mx-5 lg:mx-20 3xl:mx-40">
+            Popular Topics
+          </h1>
+          <PopularTopics />
+        </div>
+        <RecommendedArticles />
       </div>
-      <RecommendedArticles />
-
-      {/* TODO: add scroll button */}
-    </div>
+      <ScrollButton sectionId={'articles'} />
+    </>
   );
 };
 
