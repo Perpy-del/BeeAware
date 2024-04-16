a6895a80da6d3f1630e29b0baecb9f13d799c28b
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-04-16 20:53:24 +0100

    chore: initialized auth pages

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {}
+
+const SignupPage = (props: Props) => {
+  return (
+    <div>
+        <div className="bg-[url('/signup-bg.jpg')]">
+            <div className='h-90vh'>
+                Hello Sign up page
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SignupPage
