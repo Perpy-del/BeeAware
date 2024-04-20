1e0550c85ca38712add964c21d09864011e901ce
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-04-20 06:33:58 +0100

    fix: rendering dashboard if user is logged in

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,31 @@
 'use client';
 
+import { User } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { auth } from '../firebase/config';
+import { useRouter } from 'next/navigation';
+
 type Props = {};
 
 const DashboardPage = (props: Props) => {
+  const router = useRouter();
 
-  return <div>DashboardPage</div>;
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+        router.push('/auth/login'); // Redirect to login page if user is not signed in
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router]);
+
+  return user ? <div>DashboardPage</div> : null;
 };
 
 export default DashboardPage;
