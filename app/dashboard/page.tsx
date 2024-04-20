4b2e565f2ddb1a831f98ac73994ac33fed618150
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-04-20 06:08:56 +0100

    fix: deploy issues

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,10 @@
 'use client';
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../firebase/config';
-import { useRouter } from 'next/navigation';
-
-type Props = {}
+type Props = {};
 
 const DashboardPage = (props: Props) => {
-  const router = useRouter();
 
-  const [user] = useAuthState(auth);
-  const userSession = JSON.parse(sessionStorage.getItem('user') as string)
+  return <div>DashboardPage</div>;
+};
 
-  if (!user && !userSession) router.push('/auth/login');
-  return (
-    <div>DashboardPage</div>
-  )
-}
-
-export default DashboardPage
+export default DashboardPage;
